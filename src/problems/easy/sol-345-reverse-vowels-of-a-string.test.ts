e4d701ea8124ccc8d61bfc06351f376e9a986dc2
Mimikkk <zdancewicz.daniel@gmail.com>
2026-03-08 00:58:42 +0100

    345

--- src/problems/easy/sol-345-reverse-vowels-of-a-string.test.ts
+++ src/problems/easy/sol-345-reverse-vowels-of-a-string.test.ts
@@ -1,39 +1,40 @@
 /**
 345 - reverse-vowels-of-a-string - https://leetcode.com/problems/reverse-vowels-of-a-string/description/
 
-Given a string `s`, reverse only all the vowels in the string and return it.
+given s:
+reverse only all the vowels in the string and return it.
 
-The vowels are `'a'`, `'e'`, `'i'`, `'o'`, and `'u'`, and they can appear in both lower and upper cases, more than once.
-
-**Example 1:**
-
-**Input:** s = "IceCreAm"
-
-**Output:** "AceCreIm"
-
-**Explanation:**
-
-The vowels in `s` are `['I', 'e', 'e', 'A']`. On reversing the vowels, s becomes `"AceCreIm"`.
-
-**Example 2:**
-
-**Input:** s = "leetcode"
-
-**Output:** "leotcede"
-
-**Constraints:**
-
-*   `1 <= s.length <= 3 * 105`
-*   `s` consist of **printable ASCII** characters.
+return the string with reversed vowels.
 */
 import { describe, expect, it } from "vitest";
 
+const vowels = "aeiouAEIOU";
 function reverseVowels(s: string): string {
-    
-};
+  let left = 0;
+  let right = s.length - 1;
+
+  let chars = [...s];
+  while (left < right) {
+    while (left < right && !vowels.includes(s[left])) ++left;
+    while (left < right && !vowels.includes(s[right])) --right;
+
+    const temp = chars[left];
+    chars[left] = chars[right];
+    chars[right] = temp;
+
+    ++left;
+    --right;
+  }
+
+  return chars.join("");
+}
 
 describe("345 - reverse-vowels-of-a-string", () => {
   it("case-1", () => {
-    expect(true).toBe(true);
+    expect(reverseVowels("IceCreAm")).toBe("AceCreIm");
+  });
+
+  it("case-2", () => {
+    expect(reverseVowels("leetcode")).toBe("leotcede");
   });
 });
